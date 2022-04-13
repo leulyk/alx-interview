40294b5dbc010b028797976647b2570dbf223e9b
Leul Yiheyis <leulykpro@gmail.com>
2022-04-13 21:04:50 +0300

    implementation of the lockboxes problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+    Interview prepration exercise: Lock Boxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+        A method that determines if all boxes can be opened.
+        Boxes is a list of lists.
+        A key with the same number as a box opens that box.
+        You can assume all keys will be positive integers.
+        There can be keys that do not have boxes.
+        The first box boxes[0] is unlocked.
+        Return True if all boxes can be opened, else return False.
+    """
+    unlocked = [False for i in range(len(boxes))]
+    unlocked[0] = True
+    for idx in range(len(boxes)):
+        if unlocked[idx]:
+            for key in boxes[idx]:
+                unlocked[key] = True
+                for discovered_key in boxes[key]:
+                    unlocked[discovered_key] = True
+    return all(unlocked)
